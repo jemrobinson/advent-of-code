d87126e3d8948f925263f690e97323251da65e23
James Robinson <james.em.robinson@gmail.com>
2025-12-10 06:01:50 +0000

    :sparkles: 2025 day 10 part 2 with scipy

--- solutions/2025/day_10.py
+++ solutions/2025/day_10.py
@@ -6,8 +6,15 @@ from advent_of_code.indicator_lights import IndicatorLights
 @timed_solution(year=2025, day=10, part=1)
 def part_one() -> int:
     indicators = IndicatorLights("2025/day-10.txt")
-    return indicators.fewest_presses()
+    return indicators.fewest_presses_lights()
+
+
+@timed_solution(year=2025, day=10, part=2)
+def part_two() -> None:
+    indicators = IndicatorLights("2025/day-10.txt")
+    return indicators.fewest_presses_joltages()
 
 
 if __name__ == "__main__":
     part_one()
+    part_two()
--- test/2025/test_day_10.py
+++ test/2025/test_day_10.py
@@ -3,4 +3,9 @@ from advent_of_code.indicator_lights import IndicatorLights
 
 def test_part_one() -> None:
     indicators = IndicatorLights("2025/day-10.test.txt")
-    assert indicators.fewest_presses() == 7
+    assert indicators.fewest_presses_lights() == 7
+
+
+def test_part_two() -> None:
+    indicators = IndicatorLights("2025/day-10.test.txt")
+    assert indicators.fewest_presses_joltages() == 33
